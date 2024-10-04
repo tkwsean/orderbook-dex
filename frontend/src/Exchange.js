54d63ffb2224866846d3f4f7808bdfe7d2312db0
tkwsean <tkwsean@gmail.com>
2024-10-04 22:30:47 +0800

    Implemented all react imports

--- frontend/src/Exchange.js
+++ frontend/src/Exchange.js
@@ -2,7 +2,21 @@ import React from 'react';
 import Web3 from 'web3';
 import getEtherBalanceInWei from './components/EtherManagement/getEtherBalanceInWei';
 import withdrawEther from './components/EtherManagement/withdrawEther';
-
+import cancelBuyOrder from './components/OrderBookManagement/cancelBuyOrder';
+import cancelSellOrder from './components/OrderBookManagement/cancelSellOrder';
+import getAccountBuyOrders from './components/OrderBookManagement/getAccountBuyOrders';
+import getAccountSellOrders from './components/OrderBookManagement/getAccountSellOrders';
+import getBuyOrderbook from './components/OrderBookManagement/getBuyOrderBook';
+import getSellOrderbook from './components/OrderBookManagement/getSellOrderBook';
+import addToken from './components/TokenManagement/addToken';
+import buyToken from './components/TokenManagement/buyToken';
+import depositToken from './components/TokenManagement/depositToken';
+import getAllTokens from './components/TokenManagement/getAllTokens';
+import getBalanceForToken from './components/TokenManagement/getBalanceForToken';
+import getTokenAddress from './components/TokenManagement/getTokenAddress';
+import hasToken from './components/TokenManagement/hasToken';
+import sellToken from './components/TokenManagement/sellToken';
+import withdrawToken from './components/TokenManagement/withdrawToken';
 
 function Exchange() {
   return (
